f57c8be662a654e760e01d6b17f028bfb03404e0
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2026-02-24 22:30:14 +0530

    Upgrade dependencies.

--- app/src/main/java/srihk/alarmq/Preferences.kt
+++ app/src/main/java/srihk/alarmq/Preferences.kt
@@ -12,7 +12,7 @@ object Preferences {
     private const val KEY_NEXT_ALARM ="nextAlarm"
 
     fun setList(context: Context, list: SnapshotStateList<Int>) {
-        val prefs = context.getSharedPreferences(PREFERENCES_NAME, ComponentActivity.MODE_PRIVATE)
+        val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         val editor = prefs.edit()
 
         editor.putString(KEY_SNOOZE_LIST, list.joinToString(" "))
@@ -20,7 +20,7 @@ object Preferences {
     }
 
     fun getList(context: Context): MutableList<Int> {
-        val prefs = context.getSharedPreferences(PREFERENCES_NAME, ComponentActivity.MODE_PRIVATE)
+        val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         val s = prefs.getString(KEY_SNOOZE_LIST, "")
         val list = mutableListOf<Int>()
         if (s != null && s.isNotEmpty()) {
@@ -32,36 +32,36 @@ object Preferences {
     }
 
     fun getIsRunning(context: Context): Boolean {
-        val prefs = context.getSharedPreferences(PREFERENCES_NAME, ComponentActivity.MODE_PRIVATE)
+        val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         return prefs.getBoolean(KEY_IS_RUNNING, false)
     }
 
     fun setIsRunning(context: Context, isRunning: Boolean) {
-        val prefs = context.getSharedPreferences(PREFERENCES_NAME, ComponentActivity.MODE_PRIVATE)
+        val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         val editor = prefs.edit()
         editor.putBoolean(KEY_IS_RUNNING, isRunning)
         editor.apply()
     }
 
     fun getState(context: Context): Int {
-        val prefs = context.getSharedPreferences(PREFERENCES_NAME, ComponentActivity.MODE_PRIVATE)
+        val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         return prefs.getInt(KEY_STATE, 0)
     }
 
     fun setState(context: Context, state: Int) {
-        val prefs = context.getSharedPreferences(PREFERENCES_NAME, ComponentActivity.MODE_PRIVATE)
+        val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         val editor = prefs.edit()
         editor.putInt(KEY_STATE, state)
         editor.apply()
     }
 
     fun getNextAlarm(context: Context): String {
-        val prefs = context.getSharedPreferences(PREFERENCES_NAME, ComponentActivity.MODE_PRIVATE)
+        val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         return prefs.getString(KEY_NEXT_ALARM, "") ?: return ""
     }
 
     fun setNextAlarm(context: Context, nextAlarm: String) {
-        val prefs = context.getSharedPreferences(PREFERENCES_NAME, ComponentActivity.MODE_PRIVATE)
+        val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         val editor = prefs.edit()
         editor.putString(KEY_NEXT_ALARM, nextAlarm)
         editor.apply()
